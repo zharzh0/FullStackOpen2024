974d818bbf5830952b9d7fdf9e71cbf77aa847be
zharzh0 <viianen.ville@gmail.com>
2024-03-11 09:36:32 +0200

    Update App.jsx
    deleted unnecessary lines

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -1,57 +1,3 @@
-//exercise 1.1
-
-/*const Header = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>Number of exercises {props.exercises}</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
-    <>
-      <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total exercises={exercises1 + exercises2 + exercises3} />
-    </>
-  )
-}
-
-export default App*/
-
-//exercise 1.2
 
 const Header = (props) => {
   console.log(props)
@@ -95,8 +41,6 @@ const Total = (props) => {
     </div>
   )
 }
-
-
 
 const App = () => {
   const course = 'Half Stack application development'
