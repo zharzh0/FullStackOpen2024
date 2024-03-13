416318eb996c01244ae9b9d9a6c3af19583fab46
zharzh0 <viianen.ville@gmail.com>
2024-03-13 11:17:49 +0200

    Update App.jsx
    put the course and part into an single js object

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -1,28 +1,28 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  const totalExercises = part1.exercises + part2.exercises + part3.exercises
-
 
   return (
-
-    < div >
+    <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total total={totalExercises} />
-    </div >
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
   )
 }
 
@@ -30,28 +30,28 @@ const Header = ({ course }) => {
   console.log(course)
   return (
     <div>
-      <h1>{course}</h1>
+      <h1>{course.name}</h1>
     </div>
   )
 }
 
-const Content = ({ part1, part2, part3 }) => {
-  console.log(part1, part2, part3)
+const Content = ({ parts }) => {
+  console.log(parts)
   return (
     <div>
-      <Part part={part1} />
-      <Part part={part2} />
-      <Part part={part3} />
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </div>
   )
 }
 
-const Total = ({ total }) => {
-  console.log(total)
-
+const Total = ({ parts }) => {
+  console.log(parts)
+  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises
   return (
     <div>
-      <p>Total number of exercises: {total}</p>
+      <p>Number of exercises: {totalExercises}</p>
     </div>
   )
 }
@@ -67,5 +67,7 @@ const Part = ({ part }) => {
   )
 
 }
+
+
 
 export default App
