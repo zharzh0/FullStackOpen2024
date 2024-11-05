d5b1d177a830257f24401d5be344b3934b62f3b7
zharzh0 <viianen.ville@gmail.com>
2024-11-05 13:19:17 +0200

    tehtävä 1.6

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -1,73 +1,36 @@
+import { useState } from 'react'
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>give feedback</h1>
+      <Button handleClick={handleGood} text='good' />
+
+      <Button handleClick={handleNeutral} text='neutral' />
+
+      <Button handleClick={handleBad} text='bad' />
+      <h1>statistics</h1>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
-
-const Header = ({ course }) => {
-  console.log(course)
-  return (
-    <div>
-      <h1>{course.name}</h1>
-    </div>
-  )
-}
-
-const Content = ({ parts }) => {
-  console.log(parts)
-  return (
-    <div>
-      <Part part={parts[0]} />
-      <Part part={parts[1]} />
-      <Part part={parts[2]} />
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  console.log(parts)
-  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises
-  return (
-    <div>
-      <p>Number of exercises: {totalExercises}</p>
-    </div>
-  )
-}
-
-const Part = ({ part }) => {
-  console.log(part)
-  return (
-    < div >
-      <p>
-        {part.name} {part.exercises}
-      </p>
-    </div >
-  )
-
-}
-
-
 
 export default App
