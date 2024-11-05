422b309ed00cf796886a6f2f2add6ed8f0741bad
zharzh0 <viianen.ville@gmail.com>
2024-11-05 13:41:49 +0200

    tehtävä 1.7

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -11,10 +11,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const handleGood = () => setGood(good + 1);
-  const handleNeutral = () => setNeutral(neutral + 1);
-  const handleBad = () => setBad(bad + 1);
+  const handleGood = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+  }
 
   return (
     <div>
@@ -29,6 +41,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {all > 0 ? (good - bad) / all : 0}</p>
+      <p>positive {all > 0 ? (good / all) * 100 : 0} %</p>
     </div>
   )
 }
