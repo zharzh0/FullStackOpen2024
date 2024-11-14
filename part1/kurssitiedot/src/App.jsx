409169ff1e15b849ee60b2c47adc3521f659ff43
zharzh0 <viianen.ville@gmail.com>
2024-11-14 16:04:11 +0200

    tehtävä 1.10

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -6,6 +6,10 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const StatisticLine = ({ text, value }) => (
+  <p>{text} {value}</p>
+)
+
 const Statistics = ({ good, neutral, bad }) => {
 
   if (good === 0 && neutral === 0 && bad === 0) {
@@ -24,12 +28,13 @@ const Statistics = ({ good, neutral, bad }) => {
     <div>
       <h1>statistics</h1>
 
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <StatisticLine text='good' value={good} />
+      <StatisticLine text='neutral' value={neutral} />
+      <StatisticLine text='bad' value={bad} />
+      <StatisticLine text='all' value={all} />
+      <StatisticLine text='average' value={average} />
+      <StatisticLine text='positive' value={positive + ' %'} />
+
     </div>
   )
 }
